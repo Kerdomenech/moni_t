e7407ee75f69a3a02623e2f9aacce5485ee9f9b2
Kerlys Domenech <122834334+Kerdomenech@users.noreply.github.com>
2026-04-10 14:01:37 -0500

    Add files via upload

---
+++ escalera.py
@@ -0,0 +1,15 @@
+p=5
+
+
+while(p>=1):
+    
+    p = p-1
+    
+    if p == 4:
+        print ("bajaste un piso, segurias bajando")
+    elif p == 3:
+        print ("bajaste un piso, segurias bajando")
+    elif p == 2:
+        print ("bajaste un piso, segurias bajando")
+    else:
+        print ("bajaste al piso 1")
